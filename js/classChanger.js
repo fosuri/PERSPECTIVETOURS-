1c20dd59a7fdbd57d7da89e28a9d3bfae27f5f09
Fosuri <134892507+fosuri@users.noreply.github.com>
2024-06-04 20:13:51 +0300

    dfgfghu

--- js/classChanger.js
+++ js/classChanger.js
@@ -12,50 +12,11 @@ function checkScreenWidth() {
       container.classList.remove("why-container-reverse");
     });
 
-    reviewSection.forEach((container) => {
-      container.classList.remove("one-screen-section");
-      container.classList.add("section-space");
-    });
-
-    ticketSection.forEach((container) => {
-      container.classList.remove("one-screen-section");
-      container.classList.add("section-space");
-    });
-
-    offerSection.forEach((container) => {
-      container.classList.remove("one-screen-section");
-      container.classList.add("section-space");
-    });
   } else {
     reverseContainers.forEach((container) => {
       container.classList.add("why-container-reverse");
     });
 
-    reviewSection.forEach((container) => {
-      container.classList.add("one-screen-section");
-      container.classList.remove("section-space");
-    });
-
-    ticketSection.forEach((container) => {
-      container.classList.add("one-screen-section");
-      container.classList.remove("section-space");
-    });
-
-    offerSection.forEach((container) => {
-      container.classList.add("one-screen-section");
-      container.classList.remove("section-space");
-    });
-  }
-  if (screenWidth < 845) {
-    formSection.forEach((container) => {
-      container.classList.remove("one-screen-section");
-      container.classList.add("section-space");
-    });
-  } else {
-    formSection.forEach((container) => {
-      container.classList.add("one-screen-section");
-      container.classList.remove("section-space");
-    });
   }
 }
 
